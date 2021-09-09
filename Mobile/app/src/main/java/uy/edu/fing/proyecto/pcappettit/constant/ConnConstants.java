2e60ab5487a7f430d9885e5e967e35926afeb033
mpulidouy <mpulidouy@hotmail.com>
2021-09-09 16:12:15 -0300

    Se crea diseño móvil. Se crea el login contra el servidor

---
+++ Mobile/app/src/main/java/uy/edu/fing/proyecto/pcappettit/constant/ConnConstants.java
@@ -0,0 +1,10 @@
+package uy.edu.fing.proyecto.pcappettit.constant;
+
+public class ConnConstants {
+    //GENERAL
+    final public static String USER_AGENT = "AppEtit.Android";
+
+    //USUARIO
+    final public static String API_USRLOGIN_URL = "http://192.168.56.1:8080/codigo-web/rest/obtenerJWT";
+
+}
